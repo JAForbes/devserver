9afd8c3e4d66c55a423023e4998c02f3450bd955
James <james.a.forbes@gmail.com>
2015-01-23 18:45:00 +1100

    Serve current directory instead of parent

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
+var path = require('path');
 var app = express();
-app.use(express.static('../'));
+app.use(express.static(path.resolve('.')));
 app.listen(80);
 console.log('Src Development Server - Listening on port 80');
